4bc531a8072c354b8ced2e9a91d215daf2fc77c7
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-16 11:03:26 +0200

    use more appropriate language for building a backend (and not a script)

---
+++ m165-jukebox-webservice/src/main/java/tech/bison/trainee/M165JukeboxWebserviceApplication.java
@@ -0,0 +1,13 @@
+package tech.bison.trainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M165JukeboxWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M165JukeboxWebserviceApplication.class, args);
+	}
+
+}
