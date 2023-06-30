a86b0a34612537c080d91a431386997bcdb41fb0
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-30 10:41:51 +0200

    implement adding song to playlist

--- m165-jukebox-webservice/src/main/java/tech/bison/trainee/server/webservice/adapter/rest/PlaylistController.java
+++ m165-jukebox-webservice/src/main/java/tech/bison/trainee/server/webservice/adapter/rest/PlaylistController.java
@@ -9,11 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.bison.trainee.server.business.domain.playlist.Playlist;
+import tech.bison.trainee.server.business.domain.song.Song;
 import tech.bison.trainee.server.business.service.PlaylistService;
+import tech.bison.trainee.server.business.service.SongService;
 import tech.bison.trainee.server.webservice.adapter.model.PlaylistDto;
 import tech.bison.trainee.server.webservice.domain.WebMapperService;
 
@@ -23,10 +26,12 @@ public class PlaylistController {
 
   private final WebMapperService mapper = new WebMapperService();
   private final PlaylistService playlistService;
+  private final SongService songService;
 
   @Autowired
-  public PlaylistController(PlaylistService playlistService) {
+  public PlaylistController(PlaylistService playlistService, SongService songService) {
     this.playlistService = playlistService;
+    this.songService = songService;
   }
 
   @GetMapping()
@@ -49,5 +54,18 @@ public class PlaylistController {
   public ResponseEntity<PlaylistDto> addNewPlaylist(@RequestBody PlaylistDto dto) {
     Playlist playlist = mapper.fromDto(dto);
     return ResponseEntity.ok(mapper.toDto(playlistService.save(playlist)));
+  }
+
+  @PutMapping(path = "/{id}/{songId}")
+  public ResponseEntity<PlaylistDto> addSongToPlaylist(@PathVariable UUID id, @PathVariable UUID songId) {
+    Optional<Playlist> existingPlaylist = Optional.ofNullable(playlistService.findById(id));
+    Optional<Song> existingSong = Optional.ofNullable(songService.findById(songId));
+    if (existingPlaylist.isPresent() && existingSong.isPresent()) {
+      Playlist playlist = existingPlaylist.get();
+      playlist.getSongs().add(existingSong.get());
+      return ResponseEntity.ok(mapper.toDto(playlistService.save(playlist)));
+    } else {
+      return ResponseEntity.notFound().build();
+    }
   }
 }
